36ecd8a8d97d9de5fbb58a2e286c3aa6b51ebac8
smaegol <p.krawczyk@ibb.waw.pl>
2018-12-04 10:49:13 +0100

    added library type option and other adapter sequences

--- cutadapt_process.sh
+++ cutadapt_process.sh
@@ -24,14 +24,20 @@
 ## variables ##
 ### Global variables - do not modify
 INPUT_DIR="." # default input dir - current dir
+LIBRARY_TYPE="TruSeqHT" # default library type
+#Arrays containing adapter sequences
+declare -A adapter1=( 
+ [TruSeqHT]="AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTNNNNNNNNGTGTAGATCTCGGTGGTCGCCGTATCATT"  
+ [Nextera]="CTGTCTCTTATACACATCTGACGCTGCCGACGANNNNNNNNGTGTAGATCTCGGTGGTCGCCGTATCATT"  
+ [dUTP]="AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT"
+)
+declare -A adapter2=( 
+ [TruSeqHT]="AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNNNATCTCGTATGCCGTCTTCTGCTTG"  
+ [Nextera]="CTGTCTCTTATACACATCTCCGAGCCCACGAGACNNNNNNNNATCTCGTATGCCGTCTTCTGCTTG"  
+ [dUTP]="AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG"
+)
 
 ### other variables
-##cutadapt options
-#adapters sequences
-#TruSeqHT (double indexing)
-adapter1='AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTNNNNNNNNGTGTAGATCTCGGTGGTCGCCGTATCATT'  #for R1 reads 
-adapter2='AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNNNATCTCGTATGCCGTCTTCTGCTTG'  #for R2 reads 
-
 cutadapt_error_rate='0.1' #error rate in overlapping fragment
 cutadapt_count='2' #number of adapters
 cutadapt_overlap='3' #length of min adapter overlap
@@ -44,7 +50,7 @@ threads='20' #number of parallel threads to use
 
 
 #Get variables from command line
-while getopts ":i:t:h:" optname
+while getopts ":i:t:h:l:" optname
   do
     case "$optname" in
 	"h")
@@ -53,12 +59,23 @@ while getopts ":i:t:h:" optname
 		echo "Options :"
 		echo " -i : input directory"
 		echo " -t : threads"
+		echo " -l : library type (one of TruSeqHT (default), Nextera)"
 		echo " -h : THIS HELP"
 		echo ""
 		exit 1
 		;;
 	"i")
 		INPUT_DIR=$OPTARG
+        	;;
+	"l")
+		LIBRARY_TYPE=$OPTARG
+		if [ -n "${adapter1[$LIBRARY_TYPE]}" ]; then
+			adapter1=${adapter1[$LIBRARY_TYPE]}
+			adapter2=${adapter2[$LIBRARY_TYPE]}
+		else
+			echo "Wrong library type provided. Exiting..."
+			exit 1
+		fi
         	;;
 	"t")
         	threads=$OPTARG
@@ -74,6 +91,7 @@ while getopts ":i:t:h:" optname
       *)
       # Should not occur
 		echo "Unknown error while processing options"
+		exit 1
 		;;
    esac
 done
@@ -81,11 +99,10 @@ done
 
 #### load modules (on the server I'm using environmental modules. Will load the most recent version of cutadapt (tested on 1.18)
 module load cutadapt
+module load fastqc
 
 for R1_FILE in `find $INPUT_DIR -name "*R1*.fastq*"`
 do
-	
-	#R1_FILENAME=`expr match "$R1_FILE" '.*\/\(.*\)'`
 	R1_PREFIX=`expr match "$R1_FILE" '\(.*\)R1.*'`
 	R1_SUFFIX=`expr match "$R1_FILE" '.*R1\(.*\)'`
 	R2_FILE=$R1_PREFIX"R2"$R1_SUFFIX
@@ -93,7 +110,9 @@ do
 	R2_CUTADAPT_OUTPUT=$R1_PREFIX"R2_cutadapt_q"$min_quality"_l"$min_length".fastq.gz"
 	cutadapt_log=$R1_PREFIX"cutadapt.log"
 	echo "processing files: $R1_FILE and $R2_FILE"
-
-	cutadapt -j $threads -a $adapter1 -A $adapter2 -o $R1_CUTADAPT_OUTPUT -p $R2_CUTADAPT_OUTPUT --nextseq-trim=$min_quality -m $min_length -n $cutadapt_count -e $cutadapt_error_rate -O $cutadapt_overlap $R1_FILE $R2_FILE &> $cutadapt_log
-
+	if [ ! -e "$R2_CUTADAPT_OUTPUT" ]; then # Run cutadapt only if output is missing	
+		cutadapt -j $threads -a $adapter1 -A $adapter2 -o $R1_CUTADAPT_OUTPUT -p $R2_CUTADAPT_OUTPUT --nextseq-trim=$min_quality -m $min_length -n $cutadapt_count -e $cutadapt_error_rate -O $cutadapt_overlap $R1_FILE $R2_FILE &> $cutadapt_log
+	fi
 done
+#Perform quality check after filtering
+fastqc -t $threads --noextract $INPUT_DIR/*cutadapt*.fastq.gz
